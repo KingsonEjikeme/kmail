8869577f52d3d8e3858af8b90117adb3e852dd7d
Kingson Ejikeme <31480559+KingsonEjikeme@users.noreply.github.com>
2022-08-17 13:26:37 +0100

    first kmail commit

---
+++ kmail.js
@@ -0,0 +1,286 @@
+let mails = [
+    {
+        name: "John Doe",
+        important: false,
+        read: false,
+        subject: "Lorem ipsum dolor sit amet",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo dignissimos excepturi aperiam nulla perspiciatis laudantium aut distinctio explicabo. Unde minus error cum ipsa quidem temporibus saepe sit illo quibusdam fugiat!",
+        time: new Date("12/11/2019")
+    },
+    {
+        name: "Jane Doe",
+        important: false,
+        read: false,
+        subject: "Lorem ipsum dolor sit amet",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo dignissimos excepturi aperiam nulla perspiciatis laudantium aut distinctio explicabo. Unde minus error cum ipsa quidem temporibus saepe sit illo quibusdam fugiat!",
+        time: new Date("12/12/2015")
+    },
+    {
+        name: "John Doe",
+        important: false,
+        read: false,
+        subject: "Lorem ipsum dolor sit amet",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo dignissimos excepturi aperiam nulla perspiciatis laudantium aut distinctio explicabo. Unde minus error cum ipsa quidem temporibus saepe sit illo quibusdam fugiat!",
+        time: new Date("8/12/2019")
+    },
+    {
+        name: "Jane Doe",
+        important: false,
+        read: false,
+        subject: "Lorem ipsum dolor sit amet",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo dignissimos excepturi aperiam nulla perspiciatis laudantium aut distinctio explicabo. Unde minus error cum ipsa quidem temporibus saepe sit illo quibusdam fugiat!",
+        time: new Date("12/8/2017")
+    },
+    {
+        name: "Jonah Doelle",
+        important: false,
+        read: false,
+        subject: "Lorem ipsum dolor sit amet",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo dignissimos excepturi aperiam nulla perspiciatis laudantium aut distinctio explicabo. Unde minus error cum ipsa quidem temporibus saepe sit illo quibusdam fugiat!",
+        time: new Date("12/12/2019")
+    },
+    {
+        name: "Jonah Doelle",
+        important: false,
+        read: false,
+        subject: "Lorem ipsum dolor sit amet",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo dignissimos excepturi aperiam nulla perspiciatis laudantium aut distinctio explicabo. Unde minus error cum ipsa quidem temporibus saepe sit illo quibusdam fugiat!",
+        time: new Date("12/1/2011")
+    },
+    {
+        name: "James Don",
+        important: false,
+        read: false,
+        subject: "Lorem ipsum dolor sit amet",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo dignissimos excepturi aperiam nulla perspiciatis laudantium aut distinctio explicabo. Unde minus error cum ipsa quidem temporibus saepe sit illo quibusdam fugiat!",
+        time: new Date("5/4/2012")
+    },
+    {
+        name: "James Don",
+        important: false,
+        read: false,
+        subject: "Lorem ipsum dolor sit amet",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo dignissimos excepturi aperiam nulla perspiciatis laudantium aut distinctio explicabo. Unde minus error cum ipsa quidem temporibus saepe sit illo quibusdam fugiat!",
+        time: new Date("12/12/2019")
+    },
+    {
+        name: "James Don",
+        important: false,
+        read: false,
+        subject: "Lorem ipsum dolor sit amet",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo dignissimos excepturi aperiam nulla perspiciatis laudantium aut distinctio explicabo. Unde minus error cum ipsa quidem temporibus saepe sit illo quibusdam fugiat!",
+        time: new Date("12/12/2019")
+    },
+    {
+        name: "John Doe",
+        important: false,
+        read: false,
+        subject: "Lorem ipsum dolor sit amet",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo dignissimos excepturi aperiam nulla perspiciatis laudantium aut distinctio explicabo. Unde minus error cum ipsa quidem temporibus saepe sit illo quibusdam fugiat!",
+        time: new Date("12/12/2019")
+    },
+    {
+        name: "Jane Doe",
+        important: false,
+        read: false,
+        subject: "Lorem ipsum dolor sit amet",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo dignissimos excepturi aperiam nulla perspiciatis laudantium aut distinctio explicabo. Unde minus error cum ipsa quidem temporibus saepe sit illo quibusdam fugiat!",
+        time: new Date("12/12/2019")
+    },
+    {
+        name: "Jeremy Dee",
+        important: false,
+        read: false,
+        subject: "Lorem ipsum dolor sit amet",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo dignissimos excepturi aperiam nulla perspiciatis laudantium aut distinctio explicabo. Unde minus error cum ipsa quidem temporibus saepe sit illo quibusdam fugiat!",
+        time: new Date("12/12/2019")
+    },
+    {
+        name: "John Doe",
+        important: false,
+        read: false,
+        subject: "Lorem ipsum dolor sit amet",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo dignissimos excepturi aperiam nulla perspiciatis laudantium aut distinctio explicabo. Unde minus error cum ipsa quidem temporibus saepe sit illo quibusdam fugiat!",
+        time: new Date("12/12/2019")
+    },
+    {
+        name: "Jeremy Dee",
+        important: false,
+        read: false,
+        subject: "Lorem ipsum dolor sit amet",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo dignissimos excepturi aperiam nulla perspiciatis laudantium aut distinctio explicabo. Unde minus error cum ipsa quidem temporibus saepe sit illo quibusdam fugiat!",
+        time: new Date("12/12/2019")
+    },
+    {
+        name: "Jackson Deer",
+        important: false,
+        read: false,
+        subject: "Lorem ipsum dolor sit amet",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo dignissimos excepturi aperiam nulla perspiciatis laudantium aut distinctio explicabo. Unde minus error cum ipsa quidem temporibus saepe sit illo quibusdam fugiat!",
+        time: new Date("12/12/2019")
+    },
+    {
+        name: "Jackson Deer",
+        important: false,
+        read: false,
+        subject: "Lorem ipsum dolor sit amet",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo dignissimos excepturi aperiam nulla perspiciatis laudantium aut distinctio explicabo. Unde minus error cum ipsa quidem temporibus saepe sit illo quibusdam fugiat!",
+        time: new Date("12/12/2019")
+    },
+    {
+        name: "Jackson Deer",
+        important: false,
+        read: false,
+        subject: "Lorem ipsum dolor sit amet",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo dignissimos excepturi aperiam nulla perspiciatis laudantium aut distinctio explicabo. Unde minus error cum ipsa quidem temporibus saepe sit illo quibusdam fugiat!",
+        time: new Date("12/12/2019")
+    },
+    {
+        name: "John Doe",
+        important: false,
+        read: false,
+        subject: "Lorem ipsum dolor sit amet",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo dignissimos excepturi aperiam nulla perspiciatis laudantium aut distinctio explicabo. Unde minus error cum ipsa quidem temporibus saepe sit illo quibusdam fugiat!",
+        time: new Date("12/12/2019")
+    },
+    {
+        name: "Jackson Deer",
+        important: false,
+        read: false,
+        subject: "Lorem ipsum dolor sit amet",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo dignissimos excepturi aperiam nulla perspiciatis laudantium aut distinctio explicabo. Unde minus error cum ipsa quidem temporibus saepe sit illo quibusdam fugiat!",
+        time: new Date("12/12/2019")
+    },
+    {
+        name: "James Don",
+        important: false,
+        read: false,
+        subject: "Lorem ipsum dolor sit amet",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo dignissimos excepturi aperiam nulla perspiciatis laudantium aut distinctio explicabo. Unde minus error cum ipsa quidem temporibus saepe sit illo quibusdam fugiat!",
+        time: new Date("12/12/2019")
+    }
+];
+
+
+
+mails.forEach(mail => {
+    let mailsContainer = document.getElementById('mails');
+    let mailDiv = document.createElement('div');
+    let mailCheckbox = document.createElement('input');
+    let mailTitle = document.createElement('div');
+    let mailSender = document.createElement('div');
+    let mailTime = document.createElement('div');
+   
+
+    // mailDiv Styling
+    let mailDivStyle = document.createAttribute('class');
+    let mailDivMouseOver = document.createAttribute('onmouseover');
+    let mailDivMouseOut = document.createAttribute('onmouseout');
+    mailDivStyle.value= "row mt-3 align-items-center mail";
+    mailDivMouseOver.value = "addShadow(this)";
+    mailDivMouseOut.value = "removeShadow(this)";
+    mailDiv.setAttributeNode(mailDivMouseOver);
+    mailDiv.setAttributeNode(mailDivMouseOut);
+    mailDiv.setAttributeNode(mailDivStyle);
+
+    // checkbox Styling
+    let mailCheckboxStyle = document.createAttribute('type');
+    let mailCheckboxAttr = document.createAttribute('class');
+    mailCheckboxStyle.value= "checkbox";
+    mailCheckboxAttr.value= "fs-6 p-0 checkbox col-1";
+    mailCheckbox.setAttributeNode(mailCheckboxStyle);
+    mailCheckbox.setAttributeNode(mailCheckboxAttr);
+    mailCheckbox.style.backgroundColor ="red !important";
+
+
+    // title Styling
+    let mailTitleStyle = document.createAttribute('class');
+    mailTitleStyle.value = "text-red body-text col-6";
+    mailTitle.setAttributeNode(mailTitleStyle);
+
+    // sender Styling
+    let mailSenderStyle = document.createAttribute('class');
+    mailSenderStyle.value = "text-primary w-25 fs-6 sender col-2 me-auto";
+    mailSender.setAttributeNode(mailSenderStyle);
+
+    // time Styling
+    let mailTimeStyle = document.createAttribute('class');
+    mailTimeStyle.value = "text-danger date col-3";
+    mailTime.setAttributeNode(mailTimeStyle);
+        // console.log(mail);
+        mailSender.innerHTML=mail.name;
+        mailTitle.innerHTML=mail.subject;
+        mailTime.innerHTML=mail.time.toDateString();
+        mailDiv.appendChild(mailCheckbox);
+        mailDiv.appendChild(mailSender);
+        mailDiv.appendChild(mailTitle);
+        mailDiv.appendChild(mailTime);
+        document.getElementById('mails').appendChild(mailDiv);
+
+    // read/unread styling
+   
+    let selectAll = false;
+    let test = document.createAttribute('checked');
+    document.getElementById('selectAll').addEventListener('click',()=>{
+            mailDiv.firstChild.removeAttribute('checked');
+            if(!selectAll){
+                selectAll = !selectAll;
+                test.value = true;
+            mailDiv.firstChild.setAttributeNode(test);
+            }else{
+                mailDiv.firstChild.removeAttribute('checked');
+                selectAll = !selectAll;
+            }
+            
+       
+    });
+    document.getElementById('selectAllDropdown').addEventListener('click',()=>{
+        mailDiv.firstChild.removeAttribute('checked');
+        if(!selectAll){
+            selectAll = !selectAll;
+            test.value = true;
+        mailDiv.firstChild.setAttributeNode(test);
+        }else{
+            mailDiv.firstChild.removeAttribute('checked');
+            selectAll = !selectAll;  
+        }
+        
+   
+});
+    document.getElementById('selectRead').addEventListener('click',()=>{
+        mailDiv.firstChild.removeAttribute('checked');
+
+        if(mail.read){
+            test.value = true;
+            mailDiv.firstChild.setAttribute('checked',true);
+            // mailDiv.firstChild.setAttributeNode(test);
+            // console.log(mailDiv.firstChild);
+        }
+    });
+    document.getElementById('selectUnread').addEventListener('click',()=>{
+        mailDiv.firstChild.removeAttribute('checked');
+
+        if(!mail.read){
+            test.value = true;
+            mailDiv.firstChild.setAttribute('checked',true);
+            // mailDiv.firstChild.setAttributeNode(test);
+            // console.log(mailDiv.firstChild.createAttribute);
+        }
+    })
+   
+
+    mailDiv.addEventListener('click',()=>{
+        alert(`From: ${mail.name}\n\nSubject: ${mail.subject}\n\n Message: ${mail.body}\n\n Date: ${mail.time.toDateString()}\n\n You have read this mail, haven't you? ;)`);
+        mail.read = true;
+    })
+    
+    });
+
+
+    
+
+
+    
+    let test = document.querySelectorAll('div input[type="checkbox"]');
+    console.log(test);
+    
